9df981eb25cd5f279bb55a9ff4bdc34d73ab933e
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-08-11 22:16:58 +0900

    デフォルト画像のURLを定義した定数クラスDefaultImageUrlsを追加

---
+++ lib/constants/default_image_urls.dart
@@ -0,0 +1,12 @@
+/// デフォルト画像（読み込み失敗や画像未設定時などに表示）のURLを定義する定数クラスです。
+class DefaultImageUrls {
+  DefaultImageUrls._();
+
+  /// ユーザーアイコンのデフォルト画像URL
+  static const profile =
+      'https://secure.gravatar.com/avatar/931b4bb04a18ab8874b2114493d0ea8e';
+
+  /// タグアイコンのデフォルト画像URL
+  static const tagIcon =
+      'https://cdn.qiita.com/assets/icons/medium/missing-2e17009a0b32a6423572b0e6dc56727e.png';
+}
