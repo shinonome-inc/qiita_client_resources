af4d1260c088cc2634ff4af33e11662118bab091
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-08-11 22:33:14 +0900

    モデルクラスのプロパティ名をQiita APIの命名に統一

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,36 +6,34 @@ import '../constants/default_image_urls.dart';
 class User {
   final String id;
   final String name;
-  final String iconUrl;
+  final String profileImageUrl;
   final String description;
-  final int followingsCount;
+  final int followeesCount;
   final int followersCount;
-  final int posts;
+  final int itemsCount;
 
   const User({
     required this.id,
     required this.name,
-    required this.iconUrl,
+    required this.profileImageUrl,
     required this.description,
-    required this.followingsCount,
+    required this.followeesCount,
     required this.followersCount,
-    required this.posts,
+    required this.itemsCount,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
-    final profileImageUrl = json['profile_image_url'] as String?;
-    final iconUrl =
-        profileImageUrl != null && profileImageUrl.isNotEmpty
-            ? profileImageUrl
-            : DefaultImageUrls.profile;
     return User(
       id: json['id'],
       name: json['name'] ?? '',
-      iconUrl: iconUrl,
+      profileImageUrl:
+          json['profile_image_url'] == null || json['profile_image_url'].isEmpty
+              ? DefaultImageUrls.profile
+              : json['profile_image_url'],
       description: json['description'] ?? '',
-      followingsCount: json['followees_count'],
+      followeesCount: json['followees_count'],
       followersCount: json['followers_count'],
-      posts: json['items_count'],
+      itemsCount: json['items_count'],
     );
   }
 }
